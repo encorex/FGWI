3a29d83a1e33f653ac284989aa168b6bdfdf7afd
claus <claus@imeos.com>
2014-04-22 18:34:27 +0200

    Add Comments

--- newton.rb
+++ newton.rb
@@ -10,13 +10,11 @@ c = 128		# invest in period t2
 d = 312		# invest in period t3
 e = 100		# invest in period t4
 
-n.times do |i|
+	n.times do |i| # loop for iteration
 
-
-f 	= (a*z**4)-(b*z**3)-(c*z**2)-(d*z**1)-e
-ff  = (a*4*z**3)-(b*3*z**2)-(c*2*z**1)-d
-
-i_rate = z - (f/ff)
-z = i_rate
-puts i_rate
+		f 	= (a*z**4)-(b*z**3)-(c*z**2)-(d*z**1)-e
+		ff  = (a*4*z**3)-(b*3*z**2)-(c*2*z**1)-d
+		i_rate = z - (f/ff)
+		z = i_rate
+	puts i_rate
 end
